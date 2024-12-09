572094b631ecb6460ab209d7d4830264d70f6f7f
SisterBreannaCall <cal23023@byui.edu>
2024-12-09 11:25:44 -0700

    update

--- Catagories/scripts/jsonfetch.js
+++ Catagories/scripts/jsonfetch.js
@@ -1,6 +1,14 @@
 async function GetData()
 {
-    const response = await fetch("https://jsonvercelserver.vercel.app/posts");
+    const headers = new Headers();
+    headers.append("Accept", "application/json");
+
+    const options = {
+        method: "GET",
+        headers: headers
+    };
+
+    const response = await fetch("https://jsonvercelserver.vercel.app/posts", options);
 
     console.log(response.text);
 }
